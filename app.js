42d7d402b735edaa1c6553a913731832683744ca
movinayoob <movinayoob1993@gmail.com>
2024-02-10 21:23:57 +0400

    SCRUM-6-Authentication-DB-config

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 const express = require('express');
+const { connectToDatabase } = require('/Users/movina/Desktop/Semester_3/Devops/CW/hospital_management_system/HospitalManagementSysDEV/db/connection.js');
 const app = express();
 const port = 3000;
-
-// Define a simple route
-app.get('/', (req, res) => {
-  res.send('Hello, this is your API!');
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-app.get('/api/user', (req, res) => {
-    const items = ['Item 1', 'Item 2', 'Item 3'];
-    res.json(items);
+connectToDatabase()
+  .then(() => {
+    // Start your server or perform other actions after database connection
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch(error => {
+    console.error('Error connecting to the database:', error);
+    // Handle the error
   });
+
   
+/* const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://movinayoob1993:Libertyforme%4022@cluster0.yw903th.mongodb.net/hospitalDB?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+ */
